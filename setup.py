3e69f7fa3b79ec428b3e328cf87fcda4dedce505
Low Kian Seong <kianseong@gmail.com>
2014-06-23 06:06:46 +0800

    Update setup.py
    Adding a line of description to prevent project description showing up as "UNKNOWN" in pypi listing and pip search results.

--- setup.py
+++ setup.py
@@ -3,6 +3,8 @@ from setuptools import setup, find_packages
 setup(
     name='helga-crickets',
     version='0.0.1',
+    description=('Helga plugin that will respond with the word crickets' 
+                 'after a question has gone unacknowledged for a configurable amount of time.'),
     author="Shaun Duncan",
     author_email="shaun.duncan@gmail.com",
     url="https://github.com/shaunduncan/helga-crickets",
