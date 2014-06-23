b79cda1b34215e80071df23b6400c745c5aea920
Shaun Duncan <shaun.duncan@gmail.com>
2014-06-23 08:04:34 -0400

    Fixed packaging and bumped version

--- setup.py
+++ setup.py
@@ -2,13 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='helga-crickets',
-    version='0.0.2',
+    version='0.0.3',
     description=('Helga plugin that will respond with the word crickets'
                  'after a question has gone unacknowledged for a configurable amount of time.'),
     author="Shaun Duncan",
     author_email="shaun.duncan@gmail.com",
     url="https://github.com/shaunduncan/helga-crickets",
     packages=find_packages(),
+    py_modules=['helga_crickets'],
     include_package_data=True,
     zip_safe=True,
     entry_points=dict(
